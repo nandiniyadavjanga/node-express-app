a4b68d9c2cad68fe88b7bddb9a348eace90782eb
Nandini Yadav Janga <yadavnandini97@gmail.com>
2019-03-08 16:25:14 -0600

    added app.get and few console.logs

--- app.js
+++ app.js
@@ -4,34 +4,33 @@ const app = express()
 const hostname = '0.0.0.0'   // set constants
 const port = 3002
 
-app.post('/', function (req, res) {
-  console.log('POST request')
-  res.send('Welcome home!')
+app.get('/', function (req, res) {
+  res.send('<h1>Welcome  to Nandinis home!</h1>'.fontcolor("green").italics().link("http://www.fawadshaikh.com/images/panda.jpg"))
+  
+  console.log('default page successfully opened')
 })
 
-app.use(function (req, res, next) {
-  console.log('logged')
-  next()
-})
+ app.get('/hi', (req, res) => {
+  res.send('Nandu cooks tasty food for her roommates'.fontsize(1000).fontcolor("darkblue"))
+  console.log('hi page successfully opened')
+}) 
 
-app.get('/hello', (req, res) => {
-  console.log('GET request')
-  res.send('Hello World!')
-})
-
-app.get('/big',  (req, res) =>{
-  console.log('GET request')
-  res.send('<h1>Hello World!</h1>')
-})
-
+app.get('/how',  (req, res) =>{
+  res.send('<h1>Her roommates are </h1><p1>Bhavani,Nikitha,Kavya,Mayuri</p1>'.italics().fontcolor("violet").fontsize(1000))
+  console.log('how page successfully opened')      
+  })
+  app.get('/bye', (req, res) => {
+    res.send('Bye All'.fontsize(1000).fontcolor("red"))
+    console.log('hi page successfully opened')
+  }) 
 app.get('/greeting/:id',  (req, res) =>{
-  console.log('GET request')
-  res.send('Hello! The id was ' + req.params.id)
+  res.send('Hello! The id was '.fontsize(700) + req.params.id.fontsize(700))
+  console.log('greeting page successfully opened')
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
-  console.log('GET request')
-  res.send('<h1>Yo, ' + req.params.buddy + '!</h1>')
+  res.send('<h1>Yo, '.fontsize() + req.params.buddy + '!</h1>')
+  console.log('buddy page successfully opened')
 })
 
 // handle non-existant routes
@@ -41,5 +40,6 @@ app.use((req, res, next) => {
 
 app.listen(port, hostname, () => {
   console.log(`Example app listening at http://${hostname}:${port}/`)
-  console.log('Hit CTRL-C CTRL-C to stop\n')
+  console.log('Hit CTRL-C  to stop\n')
 })
+
